81de7b7cf18a492381d992e085cdb19bbf04b9ce
slotzkar <github.students.ubc.ca>
2022-10-28 21:46:03 -0700

    added a couple basic tests for jsonreader

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,5 +1,34 @@
 package persistence;
 
-public class JsonReaderTest {
+import model.Library;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+// CITATION: from JsonSerializationDemo
+public class JsonReaderTest implements JsonTest {
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            Library library = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyWorkRoom() {
+        JsonReader reader = new JsonReader("./data/emptyLibrary.json");
+        try {
+            Library library = reader.read();
+            assertEquals("main library", library.getName());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 }
